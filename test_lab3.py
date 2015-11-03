ac1166fc0b98aa55fa58add0ce232a7cc12a71c0
HonourCode <jake.miller@mail.utoronto.ca>
2015-11-03 13:53:47 -0500

    Lab 3 Completed (in class but forgot to push until now)

--- test_lab3.py
+++ test_lab3.py
@@ -22,15 +22,65 @@ def test_months_with_31():
         assert days_in_month(item) == 31
 
 # Write a test function for the months with 30 days
-
+def test_months_with_30():
+    """
+    Test months with 30 days
+    """
+    for item in MONTHS_WITH_30:
+        assert days_in_month(item) == 30
 
 # Write a test function for the months with 28 or 29 days
-
+def test_months_with_28_or_29():
+    """
+    Test months with 28 or 29 days
+    """
+    for item in MONTHS_WITH_28_or_29:
+        assert days_in_month(item) == "28 or 29"
 
 # Write a test function for months that are not capitalized properly
 # Hint: use the lower() string method
 
+def test_lower_months_with_31():
+    """
+    Test months with 31 days with lowercase entry
+    """
+    for item in MONTHS_WITH_31:
+        item.lower()
+        assert days_in_month(item) == 31
+
+
 # Write a test function for unexpected input
 # Hint: use a try/except block to deal with the exception
 # Hint: use data types other than strings as input
+"""
+try:
+    some funtion call
+except SomeError:
+        We get here because an exception was raised
+        assert False
+"""
 
+
+def test_wrong_strings():
+    """
+    Test for incorrect strings
+    """
+
+    wrong_strings = ["this", "can't", "be", "right", "12345"]
+    for item in wrong_strings:
+        try:
+            days_in_month(item)
+        except ValueError:
+            assert True
+
+
+def test_wrong_attribute():
+    """
+    Test for wrong data types
+    """
+    wrong_attributes = [1, 4.5, False, ["january", "March"]]
+    for item in wrong_attributes:
+        try:
+            days_in_month(item) == False
+        except AttributeError:
+            assert True
